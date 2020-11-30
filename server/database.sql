321d2cfda82b2962881a9e8e3562ce56369f63f6
Locasaur <jvincent.1696@gmail.com>
2020-11-30 15:13:05 -0500

    typo: db table name

--- server/database.sql
+++ server/database.sql
@@ -1,10 +1,13 @@
 --used to create postgres database
 CREATE DATABASE pern_todos;
 
-CREATE TABLE todo (
+CREATE TABLE todos (
   todo_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
   user_id VARCHAR(80) NOT NULL,
   description VARCHAR(255),
   id_edited BOOLEAN DEFAULT false,
   last_edited DATE DEFAULT NULL
 );
+
+ALTER TABLE todos
+ADD is_completed BOOLEAN DEFAULT false;
