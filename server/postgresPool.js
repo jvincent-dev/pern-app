d5acfff6c5b3c49f46ec54d514578b837383e814
Locasaur <jvincent.1696@gmail.com>
2020-12-21 19:50:14 -0500

    add dotenv

--- server/postgresPool.js
+++ server/postgresPool.js
@@ -2,9 +2,9 @@ const { Pool } = require('pg')
 
 const pool = new Pool({
   user: 'postgres',
-  password: 'TODO: Add Password',
+  password: process.env.DB_PASS,
   database: 'pern_todo',
-  host: 'localhost',
+  host: process.env.DB_HOST,
   port: 5432
 })
 
