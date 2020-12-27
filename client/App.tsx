7b569df10529cd4cbed39da956d9fe00434696e7
Locasaur <jvincent.1696@gmail.com>
2020-12-27 00:11:53 -0500

    add sentry-expo

--- client/App.tsx
+++ client/App.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import NavigationStack from './navigation/NavigationStack'
 import { NavigationContainer } from '@react-navigation/native'
 import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper' 
+import * as Sentry from "sentry-expo"
 
 const theme = {
   ...DefaultTheme,
@@ -12,6 +13,15 @@ const theme = {
 }
 
 export default function App() {
+
+  useEffect(() => {
+    Sentry.init({
+      dsn: 'https://5e6d0e15369b4a0e9c319df196ee56fa@o392675.ingest.sentry.io/5571670',
+      enableInExpoDevelopment: false,
+      debug: true, // Sentry will try to print out useful debugging information if something goes wrong with sending an event. Set this to `false` in production.
+    })
+  }, [])
+
   return (
     <NavigationContainer>
       <PaperProvider theme={theme}>
