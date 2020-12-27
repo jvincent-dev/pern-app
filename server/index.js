2700f97a310ead5fdecb1aca10f2403cdfbc520e
Locasaur <jvincent.1696@gmail.com>
2020-12-27 17:02:14 -0500

    add sentry

--- server/index.js
+++ server/index.js
@@ -1,16 +1,23 @@
 require('dotenv').config()
 
+const Sentry = require('@sentry/node')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const PORT = process.env.PORT || 5000
 
 // middleware
+process.env.NODE_ENV === 'production' && Sentry.init({ dsn: 'https://161e18d8cb2f4988ac93ae1747910b92@o392675.ingest.sentry.io/5572111' })
+
+app.use(Sentry.Handlers.requestHandler())
+
 app.use(cors())
 
 app.use(express.json())
 
 app.use('/api/todo', require('./routes/api/todo'))
+
+app.use(Sentry.Handlers.errorHandler())
 
 // routes
 app.get('/', (_, res) => res.send('<h1 style="font-family: sans-serif;">Hello World!</h1>'))
