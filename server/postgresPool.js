bb5a84d5254a56b429275cdedffd5236a69fa903
Locasaur <jvincent.1696@gmail.com>
2020-12-27 16:03:33 -0500

    update database connection config

--- server/postgresPool.js
+++ server/postgresPool.js
@@ -1,11 +1,14 @@
 const { Pool } = require('pg')
-
-const pool = new Pool({
+const config = process.env.NODE_ENV === 'production'
+ ? { connectionString: process.env.DATABASE_URL }
+ : {
   user: 'postgres',
   password: process.env.DB_PASS,
   database: 'pern_todo',
   host: process.env.DB_HOST,
   port: 5432
-})
+}
+
+const pool = new Pool(config)
 
 module.exports = pool
