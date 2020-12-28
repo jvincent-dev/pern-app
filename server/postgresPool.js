9bd6b52894e05d4bec191329fb91cd8931eacbd4
Locasaur <jvincent.1696@gmail.com>
2020-12-28 14:35:42 -0500

    take out dev config

--- server/postgresPool.js
+++ server/postgresPool.js
@@ -1,13 +1,5 @@
 const { Pool } = require('pg')
-const config = process.env.NODE_ENV === 'production'
- ? { connectionString: process.env.DATABASE_URL }
- : {
-  user: 'postgres',
-  password: process.env.DB_PASS,
-  database: 'pern_todo',
-  host: process.env.DB_HOST,
-  port: 5432
-}
+const config = { connectionString: process.env.DATABASE_URL }
 
 const pool = new Pool(config)
 
